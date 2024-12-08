ae3fd4199b3b4c7a7e6d7a339ce090d061a4f1b2
MannyEspadaa <Minifresko1@gmail.com>
2024-12-07 19:51:10 -0500

    Add files via upload
    algo for buying and holding for a year

--- StockWatch/TradingBot.java
+++ StockWatch/TradingBot.java
@@ -2,33 +2,34 @@ public class TradingBot {
     double balance;
     int sharesOwned = 0;
     double entry = 0;
-    double previousRSI = 0;
-    private static final int MA_PERIOD = 50;
+    int nextSell = -1;
+    int weeks = 52;
+
     public TradingBot(double initialBalance) {
         this.balance = initialBalance;
     }
+
     public void tradeMaker(StockReader stockBot, int currentIndex) {
         double currentPrice = stockBot.stockDataList.get(currentIndex).price;
-        double currentRSI = stockBot.calculateRSI(currentIndex);
-        double movingAverage = stockBot.MA(MA_PERIOD);
-        if (currentRSI < previousRSI && sharesOwned == 0) {
-            double amountToInvest = balance * 0.25; 
+        if (sharesOwned == 0) {
+            double amountToInvest = balance * 0.25;
             int sharesToBuy = (int) (amountToInvest / currentPrice);
             if (sharesToBuy > 0) {
                 sharesOwned += sharesToBuy;
                 entry = currentPrice;
                 balance -= sharesToBuy * currentPrice;
-                System.out.printf("Buying %d shares at $%.2f. New balance: $%.2f%n", sharesToBuy, currentPrice, balance);
+                nextSell = currentIndex + weeks;
+                System.out.printf("Buying %d shares at $%.2f. New balance: $%.2f.", sharesToBuy, currentPrice, balance);
             }
         } 
-        else if (currentRSI > previousRSI && sharesOwned > 0 && stockBot.MATrend(currentIndex)) {
+        else if (sharesOwned > 0 && currentIndex == nextSell) {
             balance += sharesOwned * currentPrice;
             System.out.printf("Selling %d shares at $%.2f. New balance: $%.2f%n", sharesOwned, currentPrice, balance);
             sharesOwned = 0; 
             entry = 0; 
+            nextSell = -1;
         } else {
             System.out.println("No trade executed.");
         }
-        previousRSI = currentRSI;
     }
 }
